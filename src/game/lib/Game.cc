90e36c45fe902903ee2a13658d9d58ab94f85e5f
Knoblauchpilze <prolific-programmer@tuta.com>
2026-06-24 22:29:17 +0200

    feat: Player can move!

--- src/game/lib/Game.cc
+++ src/game/lib/Game.cc
@@ -8,6 +8,9 @@ Game::Game()
 {}
 
 namespace {
+// Player speed in pixels per frame
+constexpr auto PLAYER_SPEED = 2;
+
 bool quitRequested(const EventData &data)
 {
   for (const auto &event : data.events)
@@ -24,10 +27,47 @@ bool quitRequested(const EventData &data)
 
   return false;
 }
+
+auto collectMotion(const EventData &data) -> Eigen::Vector3f
+{
+  Eigen::Vector3f motion = Eigen::Vector3f::Zero();
+
+  for (const auto &event : data.events)
+  {
+    if (event.type == SDL_EVENT_KEY_DOWN)
+    {
+      switch (event.key.key)
+      {
+        case SDLK_LEFT:
+          motion(0) -= PLAYER_SPEED;
+          break;
+        case SDLK_RIGHT:
+          motion(0) += PLAYER_SPEED;
+          break;
+        case SDLK_UP:
+          motion(1) -= PLAYER_SPEED;
+          break;
+        case SDLK_DOWN:
+          motion(1) += PLAYER_SPEED;
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  motion.normalize();
+  motion *= PLAYER_SPEED;
+
+  return motion;
+}
 } // namespace
 
 bool Game::update(const EventData &data)
 {
+  auto motion = collectMotion(data);
+  m_playerPosition += motion;
+
   return !quitRequested(data);
 }
 
