d1d160cf7f9855b7d736f0b9efc95be633e7f4fe
Knoblauchpilze <prolific-programmer@tuta.com>
2026-07-22 21:19:24 +0200

    chore: Added TODO regarding sound playback

--- src/assets/Sound.cc
+++ src/assets/Sound.cc
@@ -36,6 +36,19 @@ bool Sound::isFinished() const
 
 void Sound::update()
 {
+  // TODO: This puts the whole data of the sound in the playback queue.
+  // This means that the sound is 'finished' immediately after the first
+  // update.
+  // This means that we cannot pause/stop it because the data has already
+  // been sent to the audio card.
+  // To solve this, we would:
+  //  - either need to have several logical devices
+  //      https://wiki.libsdl.org/SDL3/SDL_PauseAudioStreamDevice
+  //      https://wiki.libsdl.org/SDL3/SDL_OpenAudioDevice
+  //  - or precisely put as many bytes as needed to be slightly 'ahead' of
+  //    the playback.
+  // Calculating the number of bytes to put is a function of the frequency
+  // of the sound, the number of channels and the format of the sound.
   SDL_PutAudioStreamData(m_stream, m_data, m_lengthInBytes);
 }
 
